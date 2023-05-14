9f99b70e3ce5ccdbabccae9ab318fb802bbc47ea
hoshilzy1026 <125486975+hoshilzy1026@users.noreply.github.com>
2023-05-14 12:12:21 +0800

    Add files via upload

---
+++ day_00/test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+	int i ;
+	int j ;
+	char arr1[] = "abc";
+	char arr2[] = {'a','b','c'};
+	int sz1 = sizeof(arr1) / sizeof(arr1[0]);
+	int sz2 = sizeof(arr2) / sizeof(arr2[0]);
+	return 0;
+}
---
+++ day_00/test_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#define N 26000 //超出了单个函数的栈空间限制
+int main()
+{
+	int arr[N] = { 0 };
+
+}
